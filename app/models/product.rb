7bf60500a602b2974d303af3c1116de4fabccd92
Yana Mancevich <yaninamantsevich@mail.ru>
2019-01-06 19:42:39 +0200

    first commit

---
+++ app/models/product.rb
@@ -0,0 +1,14 @@
+class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+  with: %r{\.(gif|jpg|png)\Z}i,
+  message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
+  }
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
+
+end
