312007542f7c395dfb1144c11f07f6b7213829d2
Yana Mancevich <yaninamantsevich@mail.ru>
2019-01-08 12:46:14 +0200

    Оформление заказа

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
   resources :carts
   get 'store/index'
+
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.create!(title: 'Programming Ruby  2.0',
+
+
+Product.create!(title: 'TPython for Software Design',
 description:
 %{<p>
-Ruby is the fastest growing and most exciting dynamic language
-out there. If you need to get working programs delivered fast,
-you should add Ruby to your life.
+Python for Software Design is a concise introduction to software design using the Python programming language. Intended for people with no programming experience, this book starts with the most basic concepts and gradually adds new material. Some of the ideas students find most challenging, like recursion and object-oriented programming, are divided into a sequence of smaller steps and introduced over the course of several chapters. 
 </p>},
-image_url: 'http://bookwebmaster.narod.ru/img/RubyObjectorientedMetz250.jpg',
-price: 49.95)
+image_url: 'http://greenteapress.com/thinkpython/978052172596501_small.png',
+price: 37.00)
+
+Product.create!(title: 'The Art of Software Testing, 3rd Edition ',
+description:
+%{<p>
+The hardware and software of computing have changed markedly in the three decades since the first edition of The Art of Software Testing, but this book's powerful underlying analysis has stood the test of time. Whereas most books on software testing target particular development techniques, languages, or testing methods, The Art of Software Testing, Third Edition provides a brief but powerful and comprehensive presentation of time-proven software testing approaches.
+</p>},
+image_url: 'https://media.wiley.com/product_data/coverImage300/62/11180319/1118031962.jpg',
+price: 167.00)
